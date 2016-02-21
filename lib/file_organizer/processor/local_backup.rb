39fdca66f5cf9135047e45dede38cf74b604d762
Tomas Valent <equivalent@eq8.eu>
2016-02-21 20:01:35 +0000

    processor trackers

--- lib/file_organizer/processor/local_backup.rb
+++ lib/file_organizer/processor/local_backup.rb
@@ -1,10 +1,11 @@
 module FileOrganizer
   module Processor
     class LocalBackup
-      attr_reader :destination
+      attr_reader :destination, :trackers
 
-      def initialize(destination:)
+      def initialize(destination:, trackers: [])
         @destination = destination
+        @trackers = trackers
       end
 
       def process(document:, guid:)
@@ -14,19 +15,29 @@ module FileOrganizer
 
       private
         def copy_to_destination(document:, guid:)
-          dfn = destination_filename(document: document, guid: guid)
+          dfn = destination_path(document: document, guid: guid)
           begin
             FileUtils.cp(document.pathname, dfn)
-            # success
+            status = 'success'
+            message = nil
           rescue Errno::ENOENT => e
-            raise e
-            # error e
+            status = 'error'
+            message = "#{e.class}: #{e.to_s}"
           end
+
+          notify_trackers({
+            guid: guid,
+            origin_path: document.pathname,
+            destination_path: dfn,
+            status: status,
+            message: message
+          })
         end
 
-        def destination_filename(guid:, document:)
-          destination_guid_folder(guid:guid)
-            .join(sanitize(document.pathname.basename))
+        def destination_path(guid:, document:)
+          sanitized_name = sanitize(document.pathname.basename)
+          guid_path = destination_guid_folder(guid:guid)
+          non_duplicate_path(name: sanitized_name, guid_path: guid_path)
         end
 
         def prepare_destination(guid:)
@@ -41,6 +52,31 @@ module FileOrganizer
 
         def sanitize(name)
           name.to_s.gsub(/[^0-9A-Za-z.\-_]/,'')
+        end
+
+        def generate_name(name:, suffix: )
+          pathname = Pathname.new(name)
+          pathname.sub_ext('').to_s + "-#{suffix.to_i}" + pathname.extname.to_s
+        end
+
+        def non_duplicate_path(name:, guid_path:, suffix: nil)
+          if suffix
+            file_path = guid_path.join(generate_name(name: name, suffix: suffix))
+          else
+            file_path = guid_path.join(name)
+          end
+
+          if File.exist?(file_path)
+            non_duplicate_path(name: name, guid_path: guid_path, suffix: suffix.to_i + 1)
+          else
+            file_path
+          end
+        end
+
+        def notify_trackers(*args)
+          trackers.each do |tracker|
+            tracker.track(*args)
+          end
         end
     end
   end
--- spec/processor/local_backup_spec.rb
+++ spec/processor/local_backup_spec.rb
@@ -6,26 +6,178 @@ RSpec.describe FileOrganizer::Processor::LocalBackup do
                                          # without guid_folder in list)
   end
 
+  def trigger
+    processor.process(document: document, guid: guid)
+  end
+
   let(:processor) { described_class.new(destination: destination) }
   let(:destination) { AppTest.tmp_test_upload_folder_path }
   let(:guid) { '6eaec6511eec985c9614d97d2d03252d' }
-  let(:document) do
-    path = AppTest.test_root_path.join(guid, %q{wierd#'ame.xyz.txt})
-    double(pathname: path)
+
+  let(:tracker1) { spy }
+  let(:tracker2) { spy }
+
+  before do
+    processor.trackers << tracker1
+    processor.trackers << tracker2
   end
 
-  let(:trigger) { processor.process(document: document, guid: guid) }
-
   describe '#process' do
-    it do
-      fn = Pathname
-        .new(destination)
-        .join('6eaec6511eec985c9614d97d2d03252d', 'wierdame.xyz.txt')
+    context 'when file with special chars in name' do
+      let(:document) do
+        path = AppTest.test_root_path.join(guid, %q{wierd#'ame.xyz.txt})
+        double(pathname: path)
+      end
 
-      expect(fn).not_to be_exist
-      trigger
+      let(:destination_file) do
+        Pathname
+          .new(destination)
+          .join('6eaec6511eec985c9614d97d2d03252d')
+          .join('wierdame.xyz.txt')
+          .to_s
+      end
 
-      expect(destination_files).to include(fn.to_s)
+      it 'will rename file without special chars' do
+        expect(destination_files).not_to include(destination_file)
+        trigger
+        expect(destination_files).to include(destination_file)
+      end
+
+      it 'should notify trackers' do
+        trigger
+
+        expect(tracker1)
+          .to have_received(:track)
+          .with({
+            guid: '6eaec6511eec985c9614d97d2d03252d',
+            origin_path: document.pathname,
+            destination_path: Pathname.new(destination_file),
+            status: 'success',
+            message: nil
+          })
+
+        expect(tracker2)
+          .to have_received(:track)
+          .with({
+            guid: '6eaec6511eec985c9614d97d2d03252d',
+            origin_path: document.pathname,
+            destination_path: Pathname.new(destination_file),
+            status: 'success',
+            message: nil
+          })
+      end
+    end
+
+    context 'when file nested in a folder' do
+      let(:document) do
+        path = AppTest.test_root_path.join(guid, 'folder', 'folder', 'hello world .txt')
+        double(pathname: path)
+      end
+
+      let(:destination_file) do
+        Pathname
+          .new(destination)
+          .join('6eaec6511eec985c9614d97d2d03252d')
+          .join('helloworld.txt')
+          .to_s
+      end
+
+      it 'will upload file to root of the destination folder' do
+        expect(destination_files).not_to include(destination_file)
+        trigger
+        expect(destination_files).to include(destination_file)
+      end
+    end
+
+    context 'when file with same name exist already' do
+      let(:document) do
+        path = AppTest.test_root_path.join(guid, 'blabla.txt')
+        double(pathname: path)
+      end
+
+      let(:destination_file) do
+        Pathname
+          .new(destination)
+          .join('6eaec6511eec985c9614d97d2d03252d')
+          .join('blabla-1.txt')
+          .to_s
+      end
+
+      let(:destination_file2) do
+        Pathname
+          .new(destination)
+          .join('6eaec6511eec985c9614d97d2d03252d')
+          .join('blabla-2.txt')
+          .to_s
+      end
+
+      before do
+        FileUtils.touch Pathname
+          .new(destination)
+          .join('6eaec6511eec985c9614d97d2d03252d')
+          .tap { |path| FileUtils.mkdir_p(path) }
+          .join('blabla.txt')
+      end
+
+      it 'should generate file with sufix' do
+        expect(destination_files.size).to be 1
+        trigger
+        expect(destination_files.size).to be 2
+        expect(destination_files).to include(destination_file)
+        trigger
+        expect(destination_files.size).to be 3
+        expect(destination_files).to include(destination_file2)
+      end
+    end
+
+    context 'there was error' do
+      let(:document) do
+        path = AppTest.test_root_path.join(guid, 'blabla.txt')
+        double(pathname: path)
+      end
+
+      let(:destination_file) do
+        Pathname
+          .new(destination)
+          .join('6eaec6511eec985c9614d97d2d03252d')
+          .join('blabla.txt')
+          .to_s
+      end
+
+      before do
+        allow(FileUtils)
+          .to receive(:cp)
+          .and_raise(Errno::ENOENT)
+      end
+
+      it 'not to copy the file' do
+        trigger
+        expect(destination_files).to be_empty
+      end
+
+      it 'should notify trackers with errors' do
+        trigger
+
+        expect(tracker1)
+          .to have_received(:track)
+          .with({
+            guid: '6eaec6511eec985c9614d97d2d03252d',
+            origin_path: document.pathname,
+            destination_path: Pathname.new(destination_file),
+            status: 'error',
+            message: 'Errno::ENOENT: No such file or directory'
+          })
+
+        expect(tracker2)
+          .to have_received(:track)
+          .with({
+            guid: '6eaec6511eec985c9614d97d2d03252d',
+            origin_path: document.pathname,
+            destination_path: Pathname.new(destination_file),
+            status: 'error',
+            message: 'Errno::ENOENT: No such file or directory'
+          })
+      end
     end
   end
 end
